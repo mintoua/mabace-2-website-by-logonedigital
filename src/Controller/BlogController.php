b5aa7468f4af19a7c3c1d23f6deeae197bd2b303
NGUE EMMANUEL <ngueemmanuel.prof@gmail.com>
2022-09-04 12:50:42 +0100

    blog updated

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,20 +2,56 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Entity\CategoryPost;
+use App\Repository\PostRepository;
+use App\Repository\CategoryPostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function blog(): Response
+
+    public function __construct(
+    private PostRepository $postRepo,
+    private CategoryPostRepository $categoryPostRepo,
+    private PaginatorInterface $paginator,
+    )
     {
-        return $this->render('blog/blog.html.twig');
+        
     }
-    #[Route('/blog-1', name: 'app_blog_detail')]
-    public function blogDetail(): Response
+
+    #[Route('/blog', name: 'app_blog')]
+    public function blog(Request $req): Response
+    {  
+
+        $posts = $this->paginator->paginate($this->postRepo->findOrder(), $req->query->getInt('page', 1),9);
+
+        return $this->render('blog/blog.html.twig', ['posts'=>$posts]);
+    }
+
+    #[Route('/blog/article/{slug}', name: 'app_blog_detail')]
+    public function blogDetail(Post $post): Response
     {
-        return $this->render('blog/blog-single.html.twig');
+        return $this->render('blog/blog-single.html.twig', [
+            "post"=>$post,
+            "lastPosts"=>$this->postRepo->lastPosts(),
+            "categoriesPost"=>$this->categoryPostRepo->findAll()
+        ]);
+    }
+
+
+    #[Route('/blog/thematique/{slug}', name: 'app_blog_by_category')]
+    public function blogByCategory(CategoryPost $CategoryPost, Request $req): Response
+    {
+        
+        $posts = $this->paginator->paginate($CategoryPost->getPosts(), $req->query->getInt('page', 1),5);
+        return $this->render('blog/blog-by-category.html.twig', [
+            "CategoryPost"=>$CategoryPost,
+            "posts"=>$posts
+        ]);
     }
 }
