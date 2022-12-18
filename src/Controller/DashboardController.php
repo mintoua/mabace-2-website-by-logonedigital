0a519ede38751d631f6fa3f36ad0cb959c8d2d07
NGUE EMMANUEL <ngueemmanuel.prof@gmail.com>
2022-12-18 04:20:58 +0100

    dashboard added successfully

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('espace-comptable/index.html.twig');
+    }
+
+    #[Route("/dashoard/page-blank", name:"blank_page")]
+    public function pageBlank():Response{
+        return $this->render("espace-comptable/blank.html.twig");
+    }
+
+    #[Route("/dashoard/tontine", name:"dashboard_tontine")]
+    public function totine():Response{
+        return $this->render("espace-comptable/tontine.html.twig");
+    }
+
+    #[Route("/dashoard/emprunt", name:"app_dashboard_emprunts")]
+    public function emprunt():Response{
+
+        return $this->render("espace-comptable/emprunts.html.twig");
+    }
+
+    #[Route("/dashoard/membre", name:"app_dashboard_membre")]
+    public function membre():Response{
+
+        return $this->render("espace-comptable/membre.html.twig");
+    }
+}
